c80b55f0d6de856a4a7979ada09fdf3acc0b7dda
unknown <you@example.com>
2022-01-08 15:10:05 -0300

    React-router-dom library downgraded

--- src/Layout.js
+++ src/Layout.js
@@ -1,43 +1,20 @@
-import React from "react"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Navbar from "./components/navbar"
-import  Home  from "./views/Home"
-import { Login } from "./views/Login"
-import { NotFound } from "./views/Notfound"
-import { Profile } from "./views/Profile"
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./views/Home";
+import { Login } from "./views/Login";
+import { NotFound } from "./views/Notfound";
+import { Profile } from "./views/Profile";
 
 const Layout = () => {
-    return (
-        <BrowserRouter>
-            <Routes>    
-                <Route exact path="/profile"  element={<Profile/>}>
-                    <Profile/>
-                </Route> 
-                <Route exact path="/login"  element={<Login/>}>
-                    <Login/>
-                </Route>
-                <Route exact path="/"  element={<Home/>} >
-                    <Home/>
-                </Route>
-                <Route  element={NotFound} />
-            </Routes>
-        </BrowserRouter> 
-    );
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/profile" component={Profile} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
+  );
 };
 export default Layout;
-
-/*import {
-    BrowserRouter,
-    Routes, // instead of "Switch"
-    Route,
-    } from "react-router-dom";
-  
-  // ...
-  
-    <BrowserRouter>
-        <Routes>
-        <Route exact path="/" element={<Home />}>
-            <Home/>
-        </Route>
-        </Routes>
-    </BrowserRouter>*/
